ac556f36b2bfa8f71bc64a4679924585ff7f3848
Nathan Wolfe <nate@scotswolfie.com>
2024-05-23 23:32:10 +0100

    🚧 List files

--- src/S3DeploymentAction/Program.cs
+++ src/S3DeploymentAction/Program.cs
@@ -12,5 +12,14 @@ public class Program
     {
       Console.WriteLine($"{entry.Key} = {entry.Value}");
     }
+
+    Console.WriteLine($"Current directory: {Environment.CurrentDirectory}");
+
+    DirectoryInfo info = new(Environment.CurrentDirectory);
+
+    foreach (var file in info.GetFiles())
+    {
+      Console.WriteLine($"{file.Name} {file.UnixFileMode:X}");
+    }
   }
 }
