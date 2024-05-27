95d6fa71043e3f81e415e97545a04888f8227b59
Nathan Wolfe <nate@scotswolfie.com>
2024-05-27 02:06:26 +0100

    🔊 Further improvements to logging

--- src/S3DeploymentAction/ConsoleLogger.cs
+++ src/S3DeploymentAction/ConsoleLogger.cs
@@ -1,4 +1,6 @@
-﻿namespace S3DeploymentAction;
+﻿using System.Runtime.CompilerServices;
+
+namespace S3DeploymentAction;
 
 public enum LogLevel
 {
@@ -13,43 +15,72 @@ public static class ConsoleLogger
 {
   public static LogLevel Level { get; set; } = LogLevel.Info;
 
-  public static void Info(string message)
+  private static void WriteLog(
+    string message,
+    string callerName,
+    LogLevel level,
+    ConsoleColor? color = null)
+  {
+    ConsoleColor currentColor = Console.ForegroundColor;
+    Console.ForegroundColor = color ?? currentColor;
+
+    Console.WriteLine(
+      "[{0:u}] [{1}]: {2}: {3}",
+      DateTimeOffset.UtcNow,
+      Enum.GetName(typeof(LogLevel), level),
+      callerName,
+      message);
+
+    Console.ForegroundColor = currentColor;
+  }
+
+  public static void Info(string message, [CallerMemberName] string callerName = "")
   {
     if (Level < LogLevel.Info)
     {
       return;
     }
-
-    ConsoleColor currentColor = Console.ForegroundColor;
-    Console.ForegroundColor = ConsoleColor.Blue;
-
-    Console.WriteLine($"[{DateTimeOffset.UtcNow:u}] [INFO]: {message}");
-
-    Console.ForegroundColor = currentColor;
+    
+    WriteLog(message, callerName, LogLevel.Info, ConsoleColor.Blue);
   }
 
-  public static void Success(string message)
+  public static void Success(string message, [CallerMemberName] string callerName = "")
   {
     if (Level < LogLevel.Info)
     {
       return;
     }
-
-    ConsoleColor currentColor = Console.ForegroundColor;
-    Console.ForegroundColor = ConsoleColor.Green;
-
-    Console.WriteLine($"[{DateTimeOffset.UtcNow:u}] [INFO]: {message}");
-
-    Console.ForegroundColor = currentColor;
+    
+    WriteLog(message, callerName, LogLevel.Info, ConsoleColor.Green);
   }
 
-  public static void Verbose(string message)
+  public static void Verbose(string message, [CallerMemberName] string callerName = "")
   {
     if (Level < LogLevel.Verbose)
     {
       return;
     }
+    
+    WriteLog(message, callerName, LogLevel.Verbose);
+  }
 
-    Console.WriteLine($"[{DateTimeOffset.UtcNow:u}] [VERBOSE]: {message}");
+  public static void Warning(string message, [CallerMemberName] string callerName = "")
+  {
+    if (Level < LogLevel.Warning)
+    {
+      return;
+    }
+
+    WriteLog(message, callerName, LogLevel.Warning, ConsoleColor.Yellow);
+  }
+  
+  public static void Error(string message, [CallerMemberName] string callerName = "")
+  {
+    if (Level < LogLevel.Error)
+    {
+      return;
+    }
+
+    WriteLog(message, callerName, LogLevel.Warning, ConsoleColor.Red);
   }
 }
